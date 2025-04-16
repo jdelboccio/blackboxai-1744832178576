df1078bf7309a83ae309c33ca29e0bef771b8835
jdelboccio <94017113+jdelboccio@users.noreply.github.com>
2025-04-16 15:36:24 -0400

    Add StraticonApp/ProjectsView.swift 1744832183981

---
+++ StraticonApp/ProjectsView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct ProjectsView: View {
+    var body: some View {
+        VStack {
+            Text("Ongoing and Completed Projects")
+                .font(.largeTitle)
+                .padding()
+            Text("Project details will be displayed here.")
+                .padding()
+        }
+    }
+}
