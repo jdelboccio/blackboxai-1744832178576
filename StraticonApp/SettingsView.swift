9382371dba634d30a4659d06d5426267ee00ef48
jdelboccio <94017113+jdelboccio@users.noreply.github.com>
2025-04-16 15:36:20 -0400

    Add StraticonApp/SettingsView.swift 1744832180616

---
+++ StraticonApp/SettingsView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        VStack {
+            Text("Settings")
+                .font(.largeTitle)
+                .padding()
+            Text("User preferences and app settings will be managed here.")
+                .padding()
+        }
+    }
+}
