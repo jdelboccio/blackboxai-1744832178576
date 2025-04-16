30cebda9487022426fa052dba03d399567ae031c
jdelboccio <94017113+jdelboccio@users.noreply.github.com>
2025-04-16 15:36:25 -0400

    Add StraticonApp/HomeView.swift 1744832184898

---
+++ StraticonApp/HomeView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        VStack {
+            Text("Welcome to Straticon!")
+                .font(.largeTitle)
+                .padding()
+            Text("Company news and updates will be displayed here.")
+                .padding()
+        }
+    }
+}
