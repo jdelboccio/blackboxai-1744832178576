7e831ad501253fc5f20853893c3ebf3bff4faf6e
jdelboccio <94017113+jdelboccio@users.noreply.github.com>
2025-04-16 15:36:22 -0400

    Add StraticonApp/ResourcesView.swift 1744832181602

---
+++ StraticonApp/ResourcesView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct ResourcesView: View {
+    var body: some View {
+        VStack {
+            Text("Company Resources")
+                .font(.largeTitle)
+                .padding()
+            Text("Access to important documents and resources will be provided here.")
+                .padding()
+        }
+    }
+}
