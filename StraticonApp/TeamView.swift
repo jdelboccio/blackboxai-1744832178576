a118ee511f1bc503a6b5f04aae9c64db35a02150
jdelboccio <94017113+jdelboccio@users.noreply.github.com>
2025-04-16 15:36:23 -0400

    Add StraticonApp/TeamView.swift 1744832182846

---
+++ StraticonApp/TeamView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct TeamView: View {
+    var body: some View {
+        VStack {
+            Text("Meet Our Team")
+                .font(.largeTitle)
+                .padding()
+            Text("Team member details will be displayed here.")
+                .padding()
+        }
+    }
+}
